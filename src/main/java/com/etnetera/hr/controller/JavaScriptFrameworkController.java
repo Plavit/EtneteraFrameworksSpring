eda7b9b1ddc309dc29b723d8de079faf7364e499
Marek Szeles <marek.szeles@gmail.com>
2019-07-05 19:00:29 +0200

    init

---
+++ src/main/java/com/etnetera/hr/controller/JavaScriptFrameworkController.java
@@ -0,0 +1,30 @@
+package com.etnetera.hr.controller;
+
+import com.etnetera.hr.data.JavaScriptFramework;
+import com.etnetera.hr.repository.JavaScriptFrameworkRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Simple REST controller for accessing application logic.
+ * 
+ * @author Etnetera
+ *
+ */
+@RestController
+public class JavaScriptFrameworkController extends EtnRestController {
+
+	private final JavaScriptFrameworkRepository repository;
+
+	@Autowired
+	public JavaScriptFrameworkController(JavaScriptFrameworkRepository repository) {
+		this.repository = repository;
+	}
+
+	@GetMapping("/frameworks")
+	public Iterable<JavaScriptFramework> frameworks() {
+		return repository.findAll();
+	}
+
+}
